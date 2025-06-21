7c7491c9b568d85eab0e4d74937a74e7dc3b7434
Your Name <your.email@example.com>
2025-06-21 11:07:10 +0530

    Update variable.js
    Exercise

--- HTML/JavaScript/JS new/variable.js
+++ HTML/JavaScript/JS new/variable.js
@@ -15,3 +15,29 @@ console.log(a);
 }
 */
 //console.log(a);
+
+//const  a = 10
+//console.log(a);
+
+// let a = 10
+// a = 20
+// console.log(a);
+
+// // variable exercise 1
+
+// var Price = 100;
+// var Product = 'Iphone17';
+// var Tax = 2;
+// console.log(Product);
+// var total = (Price + Tax)
+// console.log(total);
+
+
+// variable exercise 2
+
+
+// var fruitname = 'apple';
+// var count = 10;
+// var price = 100;
+// var total = (count * price);
+// console.log(total);
